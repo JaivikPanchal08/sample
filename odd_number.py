c7c7c526fcef28d3046f77bac43c8bb4deccf528
jaivik_devops <ibmfaculty@iar.ac.in>
2025-02-06 13:33:09 +0530

    First Version release

---
+++ odd_number.py
@@ -0,0 +1,8 @@
+# Function to check if a number is odd
+def is_odd(num):
+    return num % 2 != 0
+
+# Loop through the range and print odd numbers
+for number in range(200, 301):
+    if is_odd(number):
+        print(number)
